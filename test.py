c197eed5412e74929137ae47371b3ea2bd31b491
Honore.A <honoree100@yahoo.com>
2021-07-28 16:20:46 -0500

    first git commmit

---
+++ test.py
@@ -0,0 +1,24 @@
+import math
+
+pll=[5,6,8,5,2,6,9]
+la=0
+
+for each in pll:
+	la+=each*3.14
+
+print(int(la))
+
+
+class dav:
+	pi=3.14
+	def __init__(self, num1, num2):
+		vac=0
+		for each in pll:
+			vac+= (num1+num2)**dav.pi
+		print("the value you are looking for is "+ str(int(vac)))
+
+roundd=dav(2,3)
+
+
+
+		
